2b4e8514d316d6691d4ae4e1dc9abb582ffc85da
Kosti Rytkönen <kosti.rytkonen@gmail.com>
2023-08-29 11:47:54 +0300

    Platform porting

--- core/src/space/nexel/aether/core/location/LocationProvider.scala
+++ core/src/space/nexel/aether/core/location/LocationProvider.scala
@@ -1,6 +1,8 @@
 package space.nexel.aether.core.location
 
 import space.nexel.aether.core.platform.Event
+import space.nexel.aether.core.types.Location
+import space.nexel.aether.core.types.Time
 
 object LocationProvider {
   case class LocationEvent(location: Location, accuracy: Float, time: Time) extends Event {
